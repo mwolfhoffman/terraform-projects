1e830abc975ca40074a5a003745f9e9528b94600
Michael Wolf Hoffman <michaelhoffmanj@gmail.com>
2024-02-17 14:56:03 -0700

    azure app service cosmos project

--- README.md
+++ README.md
@@ -5,10 +5,3 @@ Various terraform project.
 ## Requirements
 
 1. [Terraform](https://www.terraform.io/)
-
-## To Run:
-
-1. `cd` into project
-1. `terraform init`
-1. set path to `.aws/credentials` and `profile`
-1. `terraform apply`
---
+++ azure-app-service-cosmos/mian.tf
@@ -0,0 +1,64 @@
+provider "azurerm" {
+  features {}
+}
+
+variable "docker_image" {
+  type        = string
+  description = "docker image to deploy to app service"
+}
+
+resource "azurerm_resource_group" "vehicleservice_resource_group" {
+  name     = "vehicleservice_resource_group"
+  location = "West US"
+}
+
+
+resource "azurerm_service_plan" "vehicleservice_resource_group" {
+  name                = "vehicleservice_resource_group"
+  resource_group_name = azurerm_resource_group.vehicleservice_resource_group.name
+  location            = azurerm_resource_group.vehicleservice_resource_group.location
+  os_type             = "Linux"
+  sku_name            = "F1" # Free tier
+}
+
+
+resource "azurerm_linux_web_app" "vehicleservice_app_service" {
+  name                = "vehicleservice-appservice-plan"
+  location            = azurerm_resource_group.vehicleservice_resource_group.location
+  resource_group_name = azurerm_resource_group.vehicleservice_resource_group.name
+  service_plan_id     = azurerm_service_plan.vehicleservice_resource_group.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  site_config {
+    application_stack {
+      docker_image_name = "${var.docker_image}:latest"
+    }
+    always_on = false
+
+  }
+}
+
+resource "azurerm_cosmosdb_account" "vehicleservice_db" {
+  name                = "vehicleservice-cosmos-db"
+  location            = azurerm_resource_group.vehicleservice_resource_group.location
+  resource_group_name = azurerm_resource_group.vehicleservice_resource_group.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  consistency_policy {
+    consistency_level = "Session"
+  }
+
+  geo_location {
+    location          = "West US"
+    failover_priority = 0
+  }
+}
+
+output "cosmosdb_connection_string" {
+  sensitive = true
+  value     = azurerm_cosmosdb_account.vehicleservice_db.connection_strings[0]
+}
